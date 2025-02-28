bd13e1fdc7271efbc8d891202adad3d4742fbb41
mael-10 <maelprogrammer1@gmail.com>
2025-02-28 16:49:04 -0300

    Lern new data

--- Head Frist c#/Exercises Solved/Hello/Hello/Program.cs
+++ Head Frist c#/Exercises Solved/Hello/Hello/Program.cs
@@ -14,7 +14,22 @@ namespace HelloWorld
         private static void OperatorExamples()
         {
             int width = 3;
+            //Add more
             width++;
+
+            int height = 2 + 4;
+            int area = width * height;
+            Console.WriteLine(area + "\n");
+
+            string result = "The area";
+            //Everything turns into a text
+            result = result + " is " + area;
+            Console.WriteLine(result + "\n");
+
+            Console.WriteLine(result + " is Good ");
+
+            bool truthAvalue = true;
+            Console.WriteLine(truthAvalue);
         }
     }
 }
