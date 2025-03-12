547ff6add7f9155888b0a7dc316acfb5f57d9f4a
mael-10 <maelprogrammer1@gmail.com>
2025-03-12 17:17:40 -0300

    many code as possible rn

---
+++ C# Players Guide/Guide Begin/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Guide_Begin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What kind of thing are we talking about?");
+            //Necessary target the subject
+            string a = Console.ReadLine();
+            Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+            //Target how biggest is that problem
+            string b = Console.ReadLine();
+            //Object name program
+            string c = "of Doom 3000!";
+            Console.WriteLine("The " + b + " " + a + " " + c);
+        }
+    }
+}
