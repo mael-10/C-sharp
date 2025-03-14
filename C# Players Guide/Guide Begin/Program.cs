3f4d97337232e82b34c52cc9513b7152acca29b7
mael-10 <maelprogrammer1@gmail.com>
2025-03-13 21:05:36 -0300

    New code today

--- C# Players Guide/Guide Begin/Program.cs
+++ C# Players Guide/Guide Begin/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Guide_Begin
+﻿using System.Net.Mail;
+
+namespace Guide_Begin
 {
     internal class Program
     {
@@ -12,7 +14,20 @@
             string b = Console.ReadLine();
             //Object name program
             string c = "of Doom 3000!";
-            Console.WriteLine("The " + b + " " + a + " " + c);
+            Console.WriteLine("The " + b + " " + a + " " + c + "\n \n");
+
+            string OiBebe = a;
+            Console.WriteLine(OiBebe);
+            a = "sem mais nem menos";
+            Console.WriteLine(a);
+            Console.WriteLine(OiBebe + "\n \n");
+
+
+            int r, f, g;
+            r = f = g = 10;
+            Console.WriteLine(f);
         }
+
+       
     }
 }
