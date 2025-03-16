86d2f1dd31084f2a37362ac0d63e66e7911f2b2e
mael-10 <maelprogrammer1@gmail.com>
2025-03-15 22:45:34 -0300

    code more

--- C# Players Guide/Guide Begin/Program.cs
+++ C# Players Guide/Guide Begin/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Mail;
+﻿using System.Globalization;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Guide_Begin
 {
@@ -14,18 +16,44 @@ namespace Guide_Begin
             string b = Console.ReadLine();
             //Object name program
             string c = "of Doom 3000!";
-            Console.WriteLine("The " + b + " " + a + " " + c + "\n \n");
+            Console.WriteLine($"The {b} {a} {c} \n \n"); //It's like js template literal
 
             string OiBebe = a;
             Console.WriteLine(OiBebe);
             a = "sem mais nem menos";
             Console.WriteLine(a);
-            Console.WriteLine(OiBebe + "\n \n");
+            Console.WriteLine($"{OiBebe} \n \n");
 
 
-            int r, f, g;
+            numbersVo();
+
+        }
+
+        public static void numbersVo()
+        {
+            short r, f, g;
             r = f = g = 10;
             Console.WriteLine(f);
+
+            //Most used types is int, uint, short, ushort and roughly byte
+            ulong aVeryBigNumber = 10000L;
+            aVeryBigNumber = 10_000_000_000L; //separator makes red easier
+            aVeryBigNumber = 10_000_000_000UL;
+
+            Console.WriteLine(aVeryBigNumber);
+
+            byte a = 255;
+            byte b = 255;
+
+            //converting 2bytes into 4bytes
+            short c = (short)(a + b);
+            Console.WriteLine(c);
+
+            //Result was casted and values went lost (510 --> 254 | 9bits to 8bits)
+            byte d = (byte)(a + b);
+            Console.WriteLine(d);
+
+            
         }
 
        
