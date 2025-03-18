8f0fcc7ec78d45caf0300637ddacb354c473d65c
mael-10 <maelprogrammer1@gmail.com>
2025-03-17 22:22:08 -0300

    new code style

--- C# Players Guide/C# Begin/Program.cs
+++ C# Players Guide/C# Begin/Program.cs
@@ -1,8 +1,4 @@
-﻿using System.Globalization;
-using System.Net.Mail;
-using System.Security.Cryptography.X509Certificates;
-
-namespace Guide_Begin
+﻿namespace Guide_Begin
 {
     internal class Program
     {
@@ -24,9 +20,7 @@ namespace Guide_Begin
             Console.WriteLine(a);
             Console.WriteLine($"{OiBebe} \n \n");
 
-
             numbersVo();
-
         }
 
         public static void numbersVo()
@@ -36,9 +30,9 @@ namespace Guide_Begin
             Console.WriteLine(f);
 
             //Most used types is int, uint, short, ushort and roughly byte
-            ulong aVeryBigNumber = 10000L;
-            aVeryBigNumber = 10_000_000_000L; //separator makes red easier
-            aVeryBigNumber = 10_000_000_000UL;
+            ulong aVeryBigNumber = 1000000000000000UL; // Precisa de UL porque o valor é maior que o limite de um int
+            aVeryBigNumber = 10_000_000_000L;
+            aVeryBigNumber = 10_000_000_000UL; //separator makes red easier
 
             Console.WriteLine(aVeryBigNumber);
 
@@ -53,9 +47,22 @@ namespace Guide_Begin
             byte d = (byte)(a + b);
             Console.WriteLine(d);
 
-            
-        }
+            byte a1 = 200;
+            byte b2 = 100;
+            int resultado = a + b; // Promovido para int, resultado é 300
+            short c3 = (short)(a + b); // Conversão explícita para short - Overflow! will be 44)
 
-       
+            //good code for int
+            int a3 = 200;
+            int b4 = 100;
+            int c5 = a + b; // Sem overflow, c é 300
+            Console.WriteLine($"{c} \n \n"); // Saída: 300
+
+            byte nova = 0b00000001;
+            Console.WriteLine(nova);
+
+            int forma = 0x0F00FF;
+            Console.WriteLine(forma);
+        }
     }
 }
