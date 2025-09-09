f1d5f8a99733ba6f137aa5dc595d2055af3a7936
mael-10 <maelprogrammer1@gmail.com>
2025-09-09 17:58:51 -0300

    adicionado

--- C# Players Guide/Class/Program.cs
+++ C# Players Guide/Class/Program.cs
@@ -30,6 +30,16 @@ namespace Class
             Console.WriteLine(score3.name);
 
             int[] form = new int[3];
+
+            Score2 maisUmaVez = new Score2("Oi bebe", 50, 100);
+
+            Score3 maisUmaVez3 = new Score3("Vamos mais uma vez", 20, 30);
+
+            Score4 score4DD = new Score4("Score 4", 10, 43.5f);//chama um
+
+            Score4 score4DD5 = new Score4(); //reutiliza
+
+            Score first = new(); //Essa é uma maneira de leaving off class name. Ou seja, fica mais resumido
         }
     }
 
@@ -48,4 +58,55 @@ namespace Class
 
         public bool EarnedStar() => (points / level) > 1000;
     }
+
+    class Score2 //class is on the heap
+    {
+        public string name;
+        public int points;
+        public int level;
+
+        public Score2(string name, int points, int level) //constructores ensure that a variavel ensure that it begins in a good stats. Assigning initial values to each field is common.
+        {
+            this.name = name;
+            this.points = points;
+            this.level = level; //o this indica as variáveis globais, pois sem isso pode acontecer shadowing
+        }
+    }
+
+    class Score3 //class is on the heap
+    {
+        public string _name;
+        public int _points;
+        public int _level;
+
+        public Score3(string name, int points, int level) //constructores ensure that a variavel ensure that it begins in a good stats. Assigning initial values to each field is common.
+        {
+            _name = name;
+            _points = points;
+            _level = level; //o underscore é mais usado, mas faz o mesmo papel do this
+        }
+    }
+
+    class Score4
+    {
+        public string _name;
+        public int _points;
+        public float _level;
+
+        public Score4() : this("Unknown", 10, 43.5f) //basicamente reusa o construtor com outros parametros sem construir do zero
+        {
+            Console.WriteLine($"Será que esse é o novo construtor: {_name}");
+            Console.WriteLine($"Será que esse é o novo construtor: {_points}");
+            Console.WriteLine($"Será que esse é o novo construtor: {_level}");
+        } //isso é basicamente chamando um método mas estamos chamando um construtor
+
+        public Score4(string name, int points, float level) //resusa esse construtor
+        {
+            _name = name;
+            _points = points;
+            _level = level;
+        }
+    }
+
+    //Usar uma classe sem construtor explícito é útil quando você não se importa com valores iniciais ou quando os valores podem ser configurados diretamente depois que o objeto for criado.
 }
