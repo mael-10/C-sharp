c4f914b695667426a4fb32a1c638854004a65128
mael-10 <maelprogrammer1@gmail.com>
2026-03-02 17:02:08 -0300

    final chapter exercise

---
+++ C# Players Guide/Capther18-Class-Exercise/Capther18-Class-Exercise/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Capther18_Class_Exercise
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Arrow teste = new Arrow(ArrowHead.Wood, Fletching.Plastic, 64);
+            float finalcost = teste.GetCost();
+
+            Console.WriteLine($"Final cost thats it {finalcost}");
+
+        }
+    }
+
+    class Arrow
+    {
+        ArrowHead _arrowHead;
+        Fletching _fletching;
+        int _shaftLenght;
+
+        public Arrow() : this(default, default, 0)
+        {
+        }
+
+        public Arrow(ArrowHead arrowHead, Fletching fletching, int shaftLenght)
+        {
+            _arrowHead = arrowHead;
+            _fletching = fletching;
+            _shaftLenght = shaftLenght;
+        }
+
+        public float GetCost()
+        {
+            return (int)_arrowHead + (int)_fletching + _shaftLenght * 0.05f;
+        }
+    }
+
+    enum ArrowHead
+    {
+        Steel = 10,
+        Wood = 3,
+        Obsidian = 5
+    }
+
+    enum Fletching
+    {
+        Plastic = 10,
+        TurkeyFeathers = 5,
+        GooseFeathers = 3
+    }
+}
