094fdf9ccd4792952f9b57166728561a9294177c
mael-10 <maelprogrammer1@gmail.com>
2026-03-22 14:50:21 -0300

    adicionado init

---
+++ C# Players Guide/Init-Property/Init-Property/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Init_Property
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Produto produto = new Produto { Nome = "Oiii", Preco= 100.65m};
+
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine(produto.Preco);
+
+
+        }
+    }
+}
+
+internal class Produto
+{
+    public string Nome { get; init; }
+    public decimal Preco { get; init; }
+}
