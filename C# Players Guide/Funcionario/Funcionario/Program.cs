9b3fe2a7cfa8f5f339010d90a51eb06ac78fb847
mael-10 <maelprogrammer1@gmail.com>
2026-03-23 11:50:20 -0300

    funcionario

---
+++ C# Players Guide/Funcionario/Funcionario/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Funcionarioo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Funcionario Funcionario = new Funcionario("Repositor") { Nome = "João da Silva", Salario = 0 };
+            Funcionario.Promover("Amarrador");
+            Funcionario.Salario = 300;
+
+            Console.WriteLine($"Nome do Funcionário: {Funcionario.Nome}\n" +
+                $"Cargo: {Funcionario.Cargo}\n" +
+                $"Salario: {Funcionario.Salario}");
+        }
+    }
+
+    internal class Funcionario
+    {
+        public string Nome { get; init; }
+        public string Cargo { get; private set; }
+        public decimal Salario { get; set; }
+
+        public Funcionario(string cargo)
+        {
+            Cargo = cargo;
+        }
+
+        public void Promover(string novoCargo)
+        {
+            Cargo = novoCargo;
+        }
+
+    }
+}
